cdba20d673bdfa628952ad842bf14e9b2c7a1e5d
Rajasekharan Vengalil <avranju@gmail.com>
2019-02-25 10:06:54 +0530

    Add command line args for options

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate structopt;
 
 mod errors;
 mod music;
 mod playlist;
 
 use futures::future::Future;
+use structopt::StructOpt;
 
 use music::Client;
 use playlist::read_playlist;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "import-amusic-playlist")]
+struct Opt {
+    #[structopt(short = "t", long = "team-id")]
+    pub team_id: String,
+
+    #[structopt(short = "k", long = "key-id")]
+    pub key_id: String,
+
+    #[structopt(short = "p", long = "private-key-path")]
+    pub private_key_path: String,
+
+    #[structopt(short = "s", long = "storefront", default_value = "us")]
+    pub storefront: String,
+}
+
 fn main() {
     env_logger::init();
 
+    let opt = Opt::from_args();
+
     let client = Client::new(
-        "TEAM_ID".to_owned(),
-        "KEY_ID".to_owned(),
-        std::fs::read_to_string("/path/to/private/key/file.p8").unwrap(),
-        "us".to_owned(),
+        opt.team_id,
+        opt.key_id,
+        std::fs::read_to_string(opt.private_key_path).unwrap(),
+        opt.storefront,
     );
 
     tokio::run(
